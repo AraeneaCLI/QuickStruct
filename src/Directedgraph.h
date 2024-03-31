07aee02401a178503cc6f9db0394465681ba96e9
Vivek09Chahal <vivekchahal1156@gmail.com>
2024-03-31 19:45:09 +0530

    Fixed bugs and some improvement.
    Added "iostream" library in headerfiles.
    
    Fixed error of mat variable in Directedgraph header file

--- src/Directedgraph.h
+++ src/Directedgraph.h
@@ -1,91 +1,87 @@
-using namespace std;
+#include <iostream>
 
-int readgraph()
+const int MAX_VERTICES = 50;
+int mat[MAX_VERTICES][MAX_VERTICES];
+
+int readgraph(int vertices)
 {
-	int mat[50][50], int vertices;
 	char var;
 
-	cout << "Enter the No of Vertices:- " << endl;
-	cin >> vertices;
-	
-	for(int i=1;i<=vertices;i++)
+	std::cout << "Enter the No of Vertices: " << std::endl;
+	std::cin >> vertices;
+
+	for (int i = 1; i <= vertices; i++)
 	{
-		for(int j=1;j<=vertices;j++)
+		for (int j = 1; j <= vertices; j++)
 		{
-			if(i==j)
+			if (i == j)
 			{
-				mat[i][j]=0;
+				mat[i][j] = 0;
 			}
-			
-			cout<<"Are Vertices "<<i<<" and "<<j<<" Adjacent (Y/N)?"<<endl;
-			cin>>var;
-			
-			if(var=='Y' || var=='y')
+
+			std::cout << "Are Vertices " << i << " and " << j << " Adjacent (Y/N)?" << std::endl;
+			std::cin >> var;
+
+			if (var == 'Y' || var == 'y')
 			{
-				mat[i][j]=1;
+				mat[i][j] = 1;
 			}
-			
 			else
 			{
-				
-				mat[i][j]=0;
+				mat[i][j] = 0;
 			}
 		}
-		
 	}
-	
-	return 0;
+
+	return vertices;
 }
 
-int printgraph(int mat[50][50],int vertices)
-{	
-	cout<<"The Adjacency Matrix for Given Graph:- "<<endl;
-	
-	for(int i=1;i<=vertices;i++)
-	{
-		for(int j=1;j<=vertices;j++)
-		cout<<""<<mat[i][j];
-		cout<<""<<endl;
-	}
-	
-	return 0;
-}
-
-int creategraph()
+void printgraph(int vertices)
 {
-	int mat[50][50];
-	int vertices;
-	int indeg=0, outdeg=0;
-	
-	cout<<"Enter the Number of Vertices"<<endl;
-	cin>>vertices;
-	
-	readgraph(mat, vertices);
-	
-	for(int i=1;i<=vertices;i++)
+	std::cout << "The Adjacency Matrix for Given Graph:" << std::endl;
+
+	for (int i = 1; i <= vertices; i++)
 	{
-		for(int j=1;j<=vertices;j++)
+		for (int j = 1; j <= vertices; j++)
 		{
-			if(mat[j][i]==1)
-			indeg++;
-			
-			
+			std::cout << mat[i][j] << " ";
 		}
-		
-		for(int j=1;j<=vertices;j++)
-		{
-		if(mat[i][j]==1)
-		    outdeg++;
-		}
-		
-		cout<<""<<endl;
-		cout<<"Vertex "<<i<<endl;
-		cout<<"IN Degree "<<indeg<<endl;
-		cout<<"OUT Degree "<<outdeg<<endl;
-		
+		std::cout << std::endl;
 	}
-	
-	printgraph(mat,vertices);
-	
-	return 0;
+}
+
+void creategraph()
+{
+	int vertices;
+	int indeg = 0, outdeg = 0;
+
+	std::cout << "Enter the Number of Vertices: " << std::endl;
+	std::cin >> vertices;
+
+	vertices = readgraph(vertices);
+
+	for (int i = 1; i <= vertices; i++)
+	{
+		indeg = 0;
+		outdeg = 0;
+
+		for (int j = 1; j <= vertices; j++)
+		{
+			if (mat[j][i] == 1)
+				indeg++;
+		}
+
+		for (int j = 1; j <= vertices; j++)
+		{
+			if (mat[i][j] == 1)
+				outdeg++;
+		}
+
+		std::cout << std::endl;
+		std::cout << "Vertex " << i << std::endl;
+		std::cout << "IN Degree " << indeg << std::endl;
+		std::cout << "OUT Degree " << outdeg << std::endl;
+	}
+
+	printgraph(vertices);
 }
